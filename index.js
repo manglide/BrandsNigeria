dbf500868dad28a67e40004942a1c14743a9d3e7
manglide <adeyemisalau@gmail.com>
2018-02-13 09:52:42 +0100

    Updates to Grid System

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ app.get('/', function (req, res) {
         "GROUP BY all_products.id ORDER BY rating DESC ";
   connection.query(sql, function (error, result) {
     if (error) throw error;
-    const block = '<div class="container">';
+    const block = '<div class="container-fluid">';
     const block1 = result.map((item, index) => (
       (index % 4 == 0)
       ?
       "<div class='row'>"+
-      "<div class='paper'><div class='c15'><div class='col-sm'>"+
+      "<div class='paper' itemscope itemtype='http://schema.org/Product'><div class='c15'><div class='col'>"+
       "<div class='card bg-light mb-3'>"+
       "<div class='card-header' style='background: #fff; color:#444'>"+
       "<span itemprop='name'>"+item.productname+"</span>"+
@@ -62,62 +62,62 @@ app.get('/', function (req, res) {
       "<div class='productpagebTag' itemprop='manufacturer' itemscope itemtype='http://schema.org/manufacturer'>"+item.manufacturer.toUpperCase()+"</div>"+
       "</div>"+
       "<img itemprop='image' class='card-img-top productImage' src='"+item.productImage_1+"' title='"+item.productname+"' alt='"+item.productname+"'>"+
-        "<div class='card-body cardContent'>"+
-        "<p class='card-text'>"+item.description+"</p>"+
-          "<div class='afterRatingDiv'>"+
-          "<div class='afterRatingDivPriceCom'>"+
-          "<div class='mainpriceSmall'>N"+item.price+"</div>"+
-          "</div>"+ // End of afterRatingDivPriceCom
-          "<div class='ThumbsUp'>"+
-          "<div class='ThumbsUpChildren'>"+
-          "<a href='#' class='btn btn-success btn-lg'><span class='glyphicon glyphicon-thumbs-up fa-2x'></span></a>"+
-          "<div class='textBelowRatings'>"+item.likes+"</div>"+
-          "</div>"+ // End of ThumbsUp Children 1
-          "<div class='ThumbsUpChildren'>"+
-          "<a href='#' class='btn btn-danger btn-lg'><span class='glyphicon glyphicon-thumbs-up fa-2x'></span></a>"+
-          "<div class='textBelowRatings'>"+item.dislikes+"</div>"+
-          "</div>"+ // End of ThumbsUp Children 2
-          "</div>"+ // End of Thumbs Up
-          "<div class='cardSentiment'>"+
-          "<div class='cardTitleInnerExtendedChildren'>"+
-          "<div style='width: 20%;position: absolute;float: left;'><span><i class='material-icons' style='font-size:20px; left: 1px;'>"+item.trend+"</i></span><div class='textBelowRatingTrends'>"+item.trend_direction+"</div></div>"+
-          "</div>"+ // End of cardTitleInnerExtendedChildren
-          "</div>"+ // End of Card Sentiment
-          "<div class='SentimentsDiv'>"+
-          "<div class='cardTitleInnerExtendedChildren'>"+
-          "<div><span><i class='material-icons' style='font-size:25px;color:red'>"+item.sentiment+"</i></span><div class='textBelowRatingSentiment'>"+item.sentiment_mood+"</div></div>"+
-          "</div>"+ // End of cardTitleInnerExtendedChildren
-          "<div class=''>"+ // Start of Favorites Enclosing Div
-          "<div class='cardTitleInnerExtendedChildren likebtn'>"+
-          "<div><span><i class='material-icons' style='font-size:25px;'>favorite</i></span><div class='textBelowRatingSentiment'>Like</div></div>"+
-          "</div>"+ // End of likebtn Enclosing Div
-          "<div class='cardTitleInnerExtendedChildren sharebtn'>"+
-          "<div><span><i class='material-icons' style='font-size:25px;'>share</i></span><div class='textBelowRatingSentiment'>Share</div></div>"+
-          "</div>"+ // End of sharebtn Enclosing Div
-          "<div class='cardTitleInnerExtendedChildren commentbtn'>"+
-          "<div><span><i class='fa fa-comments-o' style='font-size:25px;'></i></span><div class='peoplebtnText'>"+item.usercomments+"</div></div>"+
-          "</div>"+ // End of commentbtn Enclosing Div
-          "<div class='cardTitleInnerExtendedChildren locationbtn'>"+
-          "<div><span><i class='material-icons' style='font-size:25px;'>location_on</i></span><div class='locationbtnText'>"+item.locationcount+"</div></div>"+
-          "</div>"+ // End of locationbtn Enclosing Div
-          "</div>"+ // End of Favorites Enclosing Div
-          "</div>"+ // End of Sentiment Div
-          "<div>"+ // Start the Ingredients Div
-          "<div class='ingredientsTitle'><small class='text-muted'>Ingredients</small></div>"+
-          "<p class='dients'>"+item.ingredients+"</p>"+
-          "</div>"+ // End the Ingredients Div
-          "</div>"+ // End the afterRatingDivs Div
-          "<a href='#' class='card-link'>"+
-          "<button type='button' class='btn-success btn-sm btn-block'>"+
-          "View</button>"+
-          "</a>"+
-        "</div>"+
+      "<div class='card-body cardContent'>"+
+      "<p class='card-text'>"+item.description+"</p>"+
+        "<div class='afterRatingDiv'>"+
+        "<div class='afterRatingDivPriceCom'>"+
+        "<div class='mainpriceSmall'>N"+item.price+"</div>"+
+        "</div>"+ // End of afterRatingDivPriceCom
+        "<div class='ThumbsUp'>"+
+        "<div class='ThumbsUpChildren'>"+
+        "<a href='#' class='btn btn-success btn-lg'><span class='glyphicon glyphicon-thumbs-up fa-2x'></span></a>"+
+        "<div class='textBelowRatings'>"+item.likes+"</div>"+
+        "</div>"+ // End of ThumbsUp Children 1
+        "<div class='ThumbsUpChildren'>"+
+        "<a href='#' class='btn btn-danger btn-lg'><span class='glyphicon glyphicon-thumbs-up fa-2x'></span></a>"+
+        "<div class='textBelowRatings'>"+item.dislikes+"</div>"+
+        "</div>"+ // End of ThumbsUp Children 2
+        "</div>"+ // End of Thumbs Up
+        "<div class='cardSentiment'>"+
+        "<div class='cardTitleInnerExtendedChildren'>"+
+        "<div style='width: 20%;position: absolute;float: left;'><span><i class='material-icons' style='font-size:20px; left: 1px;'>"+item.trend+"</i></span><div class='textBelowRatingTrends'>"+item.trend_direction+"</div></div>"+
+        "</div>"+ // End of cardTitleInnerExtendedChildren
+        "</div>"+ // End of Card Sentiment
+        "<div class='SentimentsDiv'>"+
+        "<div class='cardTitleInnerExtendedChildren'>"+
+        "<div><span><i class='material-icons' style='font-size:25px;color:red'>"+item.sentiment+"</i></span><div class='textBelowRatingSentiment'>"+item.sentiment_mood+"</div></div>"+
+        "</div>"+ // End of cardTitleInnerExtendedChildren
+        "<div class=''>"+ // Start of Favorites Enclosing Div
+        "<div class='cardTitleInnerExtendedChildren likebtn'>"+
+        "<div><span><i class='material-icons' style='font-size:25px;'>favorite</i></span><div class='textBelowRatingSentiment'>Like</div></div>"+
+        "</div>"+ // End of likebtn Enclosing Div
+        "<div class='cardTitleInnerExtendedChildren sharebtn'>"+
+        "<div><span><i class='material-icons' style='font-size:25px;'>share</i></span><div class='textBelowRatingSentiment'>Share</div></div>"+
+        "</div>"+ // End of sharebtn Enclosing Div
+        "<div class='cardTitleInnerExtendedChildren commentbtn'>"+
+        "<div><span><i class='fa fa-comments-o' style='font-size:25px;'></i></span><div class='peoplebtnText'>"+item.usercomments+"</div></div>"+
+        "</div>"+ // End of commentbtn Enclosing Div
+        "<div class='cardTitleInnerExtendedChildren locationbtn'>"+
+        "<div><span><i class='material-icons' style='font-size:25px;'>location_on</i></span><div class='locationbtnText'>"+item.locationcount+"</div></div>"+
+        "</div>"+ // End of locationbtn Enclosing Div
+        "</div>"+ // End of Favorites Enclosing Div
+        "</div>"+ // End of Sentiment Div
+        "<div>"+ // Start the Ingredients Div
+        "<div class='ingredientsTitle'><small class='text-muted'>Ingredients</small></div>"+
+        "<p class='dients'>"+item.ingredients+"</p>"+
+        "</div>"+ // End the Ingredients Div
+        "</div>"+ // End the afterRatingDivs Div
+        "<a href='#' class='card-link'>"+
+        "<button type='button' class='btn-success btn-sm btn-block'>"+
+        "View</button>"+
+        "</a>"+
       "</div>"+
+    "</div>"+
       "</div></div></div>"
       :
-      (((index % 4) - 3) == 0)
+      ((((index % 4) - 3) == 0) && ((index - index) == 0))
       ?
-      "<div class='paper' itemscope itemtype='http://schema.org/Product'><div class='c15'><div class='col-sm'>"+
+      "<div class='paper' itemscope itemtype='http://schema.org/Product'><div class='c15'><div class='col'>"+
       "<div class='card bg-light mb-3'>"+
       "<div class='card-header' style='background: #fff; color:#444'>"+
       "<span itemprop='name'>"+item.productname+"</span>"+
@@ -179,8 +179,8 @@ app.get('/', function (req, res) {
       "</div>"+
       "</div></div></div></div>"
       :
-      "<div class='paper' itemscope itemtype='http://schema.org/Product'><div class='c15'><div class='col-sm'>"+
-      "<div class='card bg-light mb-3'>"+
+      "<div class='paper' itemscope itemtype='http://schema.org/Product'><div class='c15'><div class='col'>"+
+      "<div class='card bg-light mb-3 smallCard'>"+
       "<div class='card-header' style='background: #fff; color:#444'>"+
       "<span itemprop='name'>"+item.productname+"</span>"+
       "<div itemprop='category' class='productpagebTagCategory' itemscope itemtype='http://schema.org/manufacturer'>"+item.category.toUpperCase()+"</div>"+
@@ -218,7 +218,7 @@ app.get('/', function (req, res) {
           "<div><span><i class='material-icons' style='font-size:25px;'>favorite</i></span><div class='textBelowRatingSentiment'>Like</div></div>"+
           "</div>"+ // End of likebtn Enclosing Div
           "<div class='cardTitleInnerExtendedChildren sharebtn'>"+
-          "<div><span><i class='material-icons' style='font-size:25px;'>share</i></span><div class='textBelowRatingSentiment'>Share</div></div>"+
+          "<div><span><i class='material-icons' style='font-size:22px;'>share</i></span><div class='textBelowRatingSentiment'>Share</div></div>"+
           "</div>"+ // End of sharebtn Enclosing Div
           "<div class='cardTitleInnerExtendedChildren commentbtn'>"+
           "<div><span><i class='fa fa-comments-o' style='font-size:25px;'></i></span><div class='peoplebtnText'>"+item.usercomments+"</div></div>"+
